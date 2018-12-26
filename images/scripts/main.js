e3c2e896a1e3f6d82f887a328b2a3a242ec6c911
NastyaRay <nastena75755@gmail.com>
2018-12-26 16:36:55 +0300

    victorina1.1

---
+++ images/scripts/main.js
@@ -0,0 +1,11 @@
+var myHeading = document.querySelector ('h1');
+myHeading.textContent = 'Hello world!';
+var myVariable = 'Йорк';
+var myVariable1 = 10;
+var myVariable2 = true;
+var a = '7';
+if (a === '2'){
+alert ('mmmmm')}
+ else {
+	 
+ console.log(myVariable, myVariable1, myVariable2)}
